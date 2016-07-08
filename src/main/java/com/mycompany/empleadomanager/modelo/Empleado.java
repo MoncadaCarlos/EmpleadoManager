ece3a9b1d0cac9630ba6016dcfa211355155ccd0
ITmaster <itmasterTraining@Mac-mini-de-ITmaster-2.local>
2016-07-07 21:29:44 -0300

    Hello Commit

---
+++ src/main/java/com/mycompany/empleadomanager/modelo/Empleado.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.empleadomanager.modelo;
+
+
+public class Empleado {
+    private String nombre;
+    private Integer edad;
+    private double sueldo;
+    private Long id;
+    
+}
